368748c7e0773b1f86a0a69c78f5b1b6b1aa0e0f
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-03-31 20:39:31 +0500

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Hina Khan on 31/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Hina Khan on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
